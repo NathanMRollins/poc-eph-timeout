004c065abb708e9724b8b8f4c2225a42b82e2fab
NathanMRollins <m__rollins_nathan@network.lilly.com>
2019-01-08 11:40:58 -0500

    port stuff

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ var app = express()
 
 // https://poc-eph-timeout.herokuapp.com/
 
-
+var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
     res.send('hi');
     console.log('Finale')
   })
+
+  app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
   
-  app.listen(3000)
+  // app.listen(3000)
